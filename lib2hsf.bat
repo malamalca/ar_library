1fa55722337d4b6e4f60f252e86bf82ccc61b350
Miha Nahtigal <miha.nahtigal@gmail.com>
2021-02-16 17:27:21 +0100

    Initial Commit

---
+++ lib2hsf.bat
@@ -0,0 +1,3 @@
+REM This command converts library parts to HSF from "Library" folder to "Library_src"
+
+"C:\Program Files\GRAPHISOFT\ARCHICAD 24\LP_XMLConverter.exe" l2hsf "R:\Library\ArLibrary24" Library_src
---
+++ make.bat
@@ -0,0 +1,35 @@
+rem Delete all OS specific files
+del Thumbs.db /s /q
+
+cls
+@echo off
+
+echo "ARCHICAD Library Builder"
+echo "-----------------------------------------"
+echo.
+
+set "hsf2gsmCommand=C:\Program Files\GRAPHISOFT\ARCHICAD 24\LP_XMLConverter.exe"
+
+set "passwd="
+set /p passwd="Enter password for GDL objects: "
+
+echo.
+echo Converting HSF to GSM...
+if "%passwd%" NEQ "" (set hsf2gsmCommand=%hsf2gsmCommand% -password %passwd%)
+
+set hsf2gsmCommand="%hsf2gsmCommand%" hsf2l Library_src Library
+
+rem Execute command
+%hsf2gsmCommand%
+
+rem Delete empty file needed for GIT
+rem del Library\.gitkeep /s
+
+echo.
+echo Converting GSM to LCF...
+"C:\Program Files\GRAPHISOFT\ARCHICAD 24\LP_XMLConverter.exe" createcontainer ArLibrary24.lcf Library
+
+rem Create empty file back
+rem copy NUL Library\.gitkeep
+
+PAUSE
